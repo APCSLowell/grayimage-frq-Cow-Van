e3127af5673b638e431ef3ef4d90c3950ce623a6
Calvin <calvinlu@live.com>
2024-02-16 13:03:37 -0800

    Update GrayImage.java

--- src/main/java/GrayImage.java
+++ src/main/java/GrayImage.java
@@ -25,7 +25,7 @@ public class GrayImage
     /* to be implemented in part A */
     int count = 0;
     
-    for (int[] pixelRow : inxelValues) {
+    for (int[] pixelRow : indexValues) {
       for (int pixel : pixelRow) {
         count += pixel / 255;
       }
